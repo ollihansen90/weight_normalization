1c5817cc7de96c44e21f28e57145c93ca9ea20a0
ollihansen90 <ollihansen90@gmail.com>
2021-07-26 17:47:45 +0200

    v2

--- MLP.py
+++ MLP.py
@@ -39,7 +39,7 @@ class scalable_linear(nn.Module):
     def normalize(self, old_norms=None):
         #print(self.linear.weight)
         with torch.no_grad():
-            if old_norms is not None: # TODO: Passt das mit den Dimensionen?
+            if old_norms is not None:
                 self.linear.weight.mul_(old_norms.transpose(-2,-1)) # = torch.nn.Parameter((self.weights.T*preweights).T, requires_grad=True)
             normlist = torch.linalg.norm(self.linear.weight, keepdim=True, dim=-1)
             #normlist = torch.sum(torch.abs(self.linear.weight.data)**p, dim=-1).unsqueeze(-1)**(1/p)
@@ -74,6 +74,54 @@ class MLP_normed(nn.Module):
             self.outnorms.data.mul_(old_norms.squeeze())
         return returnnorms
 
+    
+
+class scalable_linear_v2(nn.Module):
+    def __init__(self, dim_in, dim_out, bias=True):
+        super().__init__()
+        self.linear = nn.Linear(dim_in, dim_out, bias)
+        #self.scaling = nn.Parameter(torch.ones(dim_out))
+
+    def forward(self, x):
+        normlist = torch.linalg.norm(self.linear.weight, dim=-1)
+        return self.linear(x)* 1/normlist
+
+    def normalize(self, old_norms=None):
+        #print(self.linear.weight)
+        with torch.no_grad():
+            if old_norms is not None:
+                self.linear.weight.mul_(old_norms.transpose(-2,-1)) # = torch.nn.Parameter((self.weights.T*preweights).T, requires_grad=True)
+            normlist = torch.linalg.norm(self.linear.weight, keepdim=True, dim=-1)
+            #normlist = torch.sum(torch.abs(self.linear.weight.data)**p, dim=-1).unsqueeze(-1)**(1/p)
+            self.linear.weight.data.mul_(1/normlist)
+            self.linear.bias.data.mul_(normlist.squeeze())
+        return normlist
+
+
+class MLP_normed_v2(nn.Module):
+    def __init__(self, dims=[784, 256, 64, 64, 62], bias=True):
+        super().__init__()
+        self.layers = nn.ModuleList([scalable_linear_v2(dims[i], dims[i+1], bias=bias) for i in range(len(dims)-1)])
+        self.outnorms = nn.Parameter(torch.ones(dims[-1]), requires_grad=False)
+
+    def forward(self, x):
+        for layer in self.layers:
+            #normlist = torch.linalg.norm(layer.linear.weight, dim=-1)
+            x = F.relu(layer(x)) #* 1/normlist
+        return self.outnorms*x
+
+    def normalize(self):
+        old_norms = None
+        returnnorms = torch.zeros((4, 2))
+        for i, layer in enumerate(self.layers):
+            old_norms = layer.normalize(old_norms)
+            returnnorms[i,0] = torch.max(old_norms)
+            returnnorms[i,1] = torch.min(old_norms)
+        with torch.no_grad():
+            self.outnorms.data.mul_(1/self.outnorms.data)
+            self.outnorms.data.mul_(old_norms.squeeze())
+        return returnnorms
+
 
 
 class Trainer(nn.Module):
@@ -94,6 +142,7 @@ class Trainer(nn.Module):
 
 
 def NetworkGenerator():
+    yield MLP_normed_v2(bias=True)
     yield MLP_normed(bias=True)
     yield MLP(bias=True)
     yield MLP(bn=True, bias=True)
